e8b4823f8fe2ebb9bbbdf221eb8a9b45de2038d8
fathrahh <gazoon3@gmail.com>
2022-06-26 16:43:12 +0700

    update stock logic for checkbox

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 const tabListEl = document.querySelectorAll(".tab-list");
 const contentLayouts = document.querySelectorAll(".content-layout");
-
-console.log(contentLayouts);
-
-tabListEl.forEach((tab) => {
-  tab.addEventListener("click", (e) => {
-    if (tab.classList.contains("active")) return;
-    setActiveTab(e.target.dataset.tab);
-  });
-});
+const markupAll = document.querySelector("#markup-all");
+const cbMarkup = document.querySelectorAll(".js-cbMarkup");
+const cbMarkupAll = document.querySelector(".js-cbMarkup-all");
+const inputMarkups = document.querySelectorAll(".js-input-markup");
 
 const setActiveTab = (targetTab) => {
   tabListEl.forEach((tab, idx) => {
@@ -21,3 +16,42 @@ const setActiveTab = (targetTab) => {
     }
   });
 };
+
+tabListEl.forEach((tab) => {
+  tab.addEventListener("click", (e) => {
+    if (tab.classList.contains("active")) return;
+    setActiveTab(e.target.dataset.tab);
+  });
+});
+
+cbMarkup.forEach((el) => {
+  let inputMarkup;
+  inputMarkups.forEach((inputEl) => {
+    if (inputEl.dataset.markup === el.dataset.markup) {
+      inputMarkup = inputEl;
+    }
+  });
+  el.addEventListener("change", (event) => {
+    console.log("e");
+    if (event.currentTarget.checked) {
+      inputMarkup.disabled = false;
+    } else {
+      inputMarkup.disabled = true;
+    }
+  });
+});
+
+cbMarkupAll.addEventListener("change", (e) => {
+  const changeEvent = new Event("change");
+  cbMarkup.forEach((el) => {
+    if (e.target.checked) {
+      el.checked = true;
+      el.disabled = true;
+      el.dispatchEvent(changeEvent);
+    } else {
+      el.checked = false;
+      el.disabled = false;
+      el.dispatchEvent(changeEvent);
+    }
+  });
+});
