28884e968694f8daad766fcaf1c7c44594fb2ab6
fathrahh <gazoon3@gmail.com>
2022-07-04 18:28:35 +0700

    [ADD] app2

--- app2.js
+++ app2.js
@@ -5,6 +5,11 @@ const cookieGoapotik = document.querySelector("#cookie-goapotik");
 const cookieSehatQ = document.querySelector("#cookie-sehatq");
 const cookieKlikdokter = document.querySelector("#cookie-klikdokter");
 const cookieNextCloud = document.querySelector("#cookie-nextcloud");
+const datePenjualanFrom = document.querySelector("#penjualan-from");
+const datePenjualanTo = document.querySelector("#penjualan-to");
+const messageWrapper = document.querySelector(
+  ".message-validate-error-wrapper"
+);
 
 // function for set tab is active
 const setActiveTab = (targetTab) => {
@@ -19,6 +24,30 @@ const setActiveTab = (targetTab) => {
   });
 };
 
+const createMessageError = () => {
+  const p = document.createElement("p");
+  p.classList.add("message-error", "js-message-validate-error");
+  p.innerText = "Input Date anda salah";
+  return p;
+};
+
+const getUnixTime = (time) => {
+  return new Date(time).getTime();
+};
+
+const validateDate = (event) => {
+  if (!datePenjualanFrom.value || !datePenjualanTo.value) return;
+  if (
+    getUnixTime(datePenjualanFrom.value) > getUnixTime(datePenjualanTo.value)
+  ) {
+    messageWrapper.innerHTML = "";
+    event.currentTarget.value = null;
+    messageWrapper.appendChild(createMessageError());
+  } else {
+    messageWrapper.innerHTML = "";
+  }
+};
+
 const saveCredentialsToLocalStorage = () => {
   if (!rememberMe.checked) return;
   const data = {
@@ -30,6 +59,7 @@ const saveCredentialsToLocalStorage = () => {
   window.localStorage.setItem("credentials", JSON.stringify(data));
 };
 
+// Declaration Event Listener Below Here
 // event Listener for manage tab active
 tabListEl.forEach((tab) => {
   tab.addEventListener("click", (e) => {
@@ -37,6 +67,9 @@ tabListEl.forEach((tab) => {
     setActiveTab(e.target.dataset.tab);
   });
 });
+
+datePenjualanFrom.addEventListener("change", validateDate);
+datePenjualanTo.addEventListener("change", validateDate);
 
 // // event onClick Cetak Order
 // btnCetak.addEventListener("click", () => {
@@ -47,24 +80,22 @@ tabListEl.forEach((tab) => {
 //   saveCredentialsToLocalStorage();
 // });
 
-cbOrderAll.addEventListener("change", (e) => {
-  cbOrder.forEach((el) => {
-    if (e.target.checked) {
-      el.checked = true;
-    } else {
-      el.checked = false;
-    }
-  });
-});
+// cbOrderAll.addEventListener("change", (e) => {
+//   cbOrder.forEach((el) => {
+//     if (e.target.checked) {
+//       el.checked = true;
+//     } else {
+//       el.checked = false;
+//     }
+//   });
+// });
 
 this.addEventListener("load", () => {
   const credentials = JSON.parse(window.localStorage.getItem("credentials"));
   if (credentials || Object.keys(credentials).length > 0) {
-    idPos.value = credentials["id_pos"] || "";
-    passwordPos.value = credentials["password_pos"] || "";
     cookieGoapotik.value = credentials["cookie_goapotik"] || "";
     cookieSehatQ.value = credentials["cookie_sehatq"] || "";
     cookieKlikdokter.value = credentials["cookie_klikdokter"] || "";
-    cookieNextCloud.value = credentials["cookie_nextcloud"] || "";
+    // cookieNextCloud.value = credentials["cookie_nextcloud"] || "";
   }
 });
